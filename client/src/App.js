2267636c04facaef5b2b292ce294491fe7b76273
user <user@users-MacBook-Pro.local>
2018-06-07 16:35:36 +0100

    Configure client app

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function listElementFunction(props) {
+  return (
+    <li>{props.description}: {props.number} ({props.classification})</li>
+  );
+}
+
 class App extends Component {
+
+  state = {contacts: []}
+
+  componentDidMount() {
+    fetch('/contacts')
+      .then(res => res.json())
+      .then(contacts => this.setState({ contacts }));
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Contacts</h1>
+        {this.state.contacts.map(contact =>
+          <div key={contact._id}>
+            <h3>{contact.name}</h3>
+          </div>)}
       </div>
     );
   }
